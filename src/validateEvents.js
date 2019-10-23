e61f855c411b98c10409280d82ca29265907fbc9
Michael Johansen <michael.johansen@nav.no>
2019-10-23 15:01:51 +0200

    Forwarder trafikk til amplitude

---
+++ src/validateEvents.js
@@ -0,0 +1,12 @@
+const Ajv = require('ajv');
+const ajv = new Ajv();
+const validateEvent = ajv.compile(require('../schemas/event'));
+
+module.exports = function(events) {
+  const errors = [];
+  events.forEach(event => {
+    const result = validateEvent(event);
+    if (!result) errors.push(validateEvent.errors);
+  });
+  return errors;
+};
