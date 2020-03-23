2c2597a8fa4567cdd0e185a3d56ecefee63c2bd3
Andreas Skomedal <49153738+skotsj@users.noreply.github.com>
2020-03-23 15:04:19 +0100

    mask 9 digit radix 36 ID (#31)

--- src/clean-referrer-urls.js
+++ src/clean-referrer-urls.js
@@ -1,6 +1,8 @@
 const formats = [
   //  HEX and DASH 6+ usually is an ID, for example start of (faceb)ook actually contains 5 legal hex chars =)
-  { regex: /[a-f0-9\-]{6,}/ig, replace: 'masked-id' }
+  { regex: /[a-f0-9\-]{6,}/ig, replace: 'masked-id' },
+  // id format https://www.nav.no/initial/1000Ro2Fi
+  { regex: /\d[oiA-Z0-9]{8,}/g, replace: 'masked-id' }
 ]
 
 module.exports = function (user_properties) {
