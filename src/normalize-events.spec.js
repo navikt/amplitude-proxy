a1dbffdb0145ba4f4b447ccc149b67fca3bda671
Andreas Skomedal <andreas.skomedal@nav.no>
2020-03-23 13:33:22 +0100

    mask 9 digit radix 36 ID

--- src/normalize-events.spec.js
+++ src/normalize-events.spec.js
@@ -4,7 +4,9 @@ const exampleEvent = require('../examples/amplitude-event');
 describe('normalize-events', function() {
   it('should add tracking options', function() {
     const cloneEvent = Object.assign({}, exampleEvent);
-    const result = normalizeEvents([cloneEvent, cloneEvent], '155.55.51.185');
+    let events = [cloneEvent, cloneEvent]
+    events[0].user_properties.initial_referrer = 'https://www.nav.no/initial/1000Ro2Fi'
+    const result = normalizeEvents(events, '155.55.51.185');
     assert.strictEqual(result.length, 2);
     result.forEach(event => {
       assert.strictEqual(typeof event.api_properties.tracking_options, 'object');
