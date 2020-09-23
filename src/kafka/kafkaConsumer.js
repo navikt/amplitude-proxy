588005b7bf9694d0aa2eb3867a84b444bbcdc9f0
JeremiahUy <jery_3000@hotmail.com>
2020-09-23 11:28:42 +0200

    UPDATE: kafka consumer to two files.

--- src/kafka/kafkaConsumer.js
+++ src/kafka/kafkaConsumer.js
@@ -3,81 +3,6 @@ const fs = require('fs');
 const shortid = require('shortid');
 const logger = require('../utils/logger');
 
-let ingresses = [
-  {
-    "app": "enonicxp",
-    "team": "enonic",
-    "namespace": "enonic",
-    "version": "unknown",
-    "context": "prod",
-    "ingress": "https://www.nav.no/no"
-  },
-  {
-    "app": "enonicxp",
-    "team": "enonic",
-    "namespace": "enonic",
-    "version": "unknown",
-    "context": "prod",
-    "ingress": "https://www.nav.no/en"
-  },
-  {
-    "app": "enonicxp",
-    "team": "enonic",
-    "namespace": "enonic",
-    "version": "unknown",
-    "context": "prod",
-    "ingress": "https://www.nav.no/se"
-  },
-  {
-    "app": "enonicxp",
-    "team": "enonic",
-    "namespace": "enonic",
-    "version": "unknown",
-    "context": "prod",
-    "ingress": "https://www.nav.no"
-  },
-  {
-    "app": "enonicxp",
-    "team": "enonic",
-    "namespace": "enonic",
-    "version": "unknown",
-    "context": "prod",
-    "ingress": "https://tjenester.nav.no/nav-sok"
-  },
-  {
-    "app": "iaweb",
-    "team": "arbeidsgiver",
-    "namespace": "iaweb",
-    "version": "unknown",
-    "context": "prod",
-    "ingress": "https://tjenester.nav.no/iaweb"
-  },
-  {
-    "app": "dokumentinnsending",
-    "team": "teamdokumenthandtering",
-    "namespace": "dokumentinnsending",
-    "version": "unknown",
-    "context": "prod",
-    "ingress": "https://tjenester.nav.no/dokumentinnsending"
-  },
-  {
-    "app": "bidragskalkulator",
-    "team": "orphans",
-    "namespace": "dokumentinnsending",
-    "version": "unknown",
-    "context": "prod",
-    "ingress": "https://tjenester.nav.no/bidragskalkulator"
-  },
-  {
-    "app": "pensjon-pselv",
-    "team": "teampensjon",
-    "namespace": "default",
-    "version": "unknown",
-    "context": "prod",
-    "ingress": "https://tjenester.nav.no/pselv"
-  }
-]
-
 module.exports = async function () {
 
   const kafka = new Kafka({
@@ -95,29 +20,18 @@ module.exports = async function () {
   await consumer.connect()
   await consumer.subscribe({ topic: 'dataplattform.ingress-topic', fromBeginning: true })
 
+  let kafkaMessage = []
+
   await consumer.run({
     eachMessage: async ({ topic, partition, message }) => {
 
-      logger.info({
-        value: message.value.toString(),
-      })
+      // logger.info({
+      //   value: message.value.toString(),
+      // })
       const jsonMessage = JSON.parse(message.value)
-
-      const messageData = {
-        app: jsonMessage.object.metadata.name,
-        team: jsonMessage.object.metadata.labels.team,
-        namespace: jsonMessage.object.metadata.namespace,
-        version: jsonMessage.object.spec.image.split(':').pop(),
-        context: jsonMessage.cluster,
-      }
-      if (jsonMessage.object.spec.ingresses) {
-          jsonMessage.object.spec.ingresses.forEach(ingressRaw => {
-          const ingress = ingressRaw.replace(/\/$/, '');
-          ingresses.push({...messageData, ingress})
-        });
-      }
+      kafkaMessage.push(jsonMessage)
     },
   })
 
-  logger.info(ingresses)
+  return kafkaMessage;
 };
