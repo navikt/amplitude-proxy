7a7b5871793a5f904e53338bf6a63024d2479dc4
andre groseth <andregroseth@hotmail.com>
2020-10-08 10:05:52 +0200

    UPDATE: Kafka topic

--- src/kafka/kafkaConsumer.js
+++ src/kafka/kafkaConsumer.js
@@ -19,7 +19,7 @@ module.exports = async function (ingressList) {
   const consumer = kafka.consumer({ groupId: `amplitude_proxy_${process.env.NAIS_CLUSTER_NAME}_${shortid.generate()}` })
 
   await consumer.connect()
-  await consumer.subscribe({ topic: 'dataplattform.ingress-topic', fromBeginning: true })
+  await consumer.subscribe({ topic: 'dataplattform.ingress-list-topic', fromBeginning: true })
 
   await consumer.run({
     eachMessage: async ({ topic, partition, message }) => {
