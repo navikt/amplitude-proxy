1ecf1bd495ffdb183d658e0e8d141da46250bc33
andre groseth <andregroseth@hotmail.com>
2020-10-13 15:22:09 +0200

    REFACTOR: test for cluster data.

--- src/filters/add-cluster-data.spec.js
+++ src/filters/add-cluster-data.spec.js
@@ -1,22 +1,23 @@
 const assert = require('assert');
+const getIngressException = require('../data/ingressException-path')
+const ingressException = require(getIngressException())
 const generateTestEvent = require('../../test-utils/generate-test-event');
 const addClusterData = require('./add-cluster-data');
+const getIngressDate = require('../data/lookup-function')
 
 describe('add-cluster-data', function() {
+  const ingressList = new Map()
+  ingressException.forEach(data => ingressList.set(data.ingress, data))
 
   it('should enrich data when existing', function() {
-    const getIngressDataDummy = (url) => {
-      return {
-        someData: 'point',
-        url: url,
-      };
-    };
-    const testEvent = generateTestEvent()
-    testEvent.platform = 'http://localhost/foo/bar?query=param';
-    const outputEvents = addClusterData([testEvent], getIngressDataDummy);
+
+    const testEvent = generateTestEvent('auto')
+    testEvent.platform = 'https://www.nav.no/foo/bar?query=param';
+    const outputEvents = addClusterData([testEvent], getIngressDate, ingressList);
     assert.strictEqual(outputEvents[0].platform, 'Web');
-    assert.strictEqual(outputEvents[0].event_properties.someData, 'point');
-    assert.strictEqual(outputEvents[0].event_properties.hostname, 'localhost');
+    assert.strictEqual(outputEvents[0].event_properties.namespace, 'enonic');
+    assert.strictEqual(outputEvents[0].event_properties.hostname, 'www.nav.no');
+    assert.strictEqual(outputEvents[0].event_properties.context, 'prod');
     assert.strictEqual(outputEvents[0].event_properties.pagePath, '/foo/bar');
   });
 
@@ -25,7 +26,7 @@ describe('add-cluster-data', function() {
     testEvent.platform = 'http://localhost/foo/bar?query=param';
     const outputEvents = addClusterData([testEvent], () => {});
     assert.strictEqual(outputEvents[0].platform, 'Web');
-    assert.strictEqual(outputEvents[0].event_properties.someData, undefined);
+    assert.strictEqual(outputEvents[0].event_properties.namespace, undefined);
     assert.strictEqual(outputEvents[0].event_properties.hostname, 'localhost');
     assert.strictEqual(outputEvents[0].event_properties.pagePath, '/foo/bar');
   });
