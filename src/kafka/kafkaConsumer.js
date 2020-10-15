ad95ff794813b1983778ab7dcd82c0fff4b87173
JeremiahUy <jery_3000@hotmail.com>
2020-10-15 12:39:08 +0200

    CHANGE: timestamp output

--- src/kafka/kafkaConsumer.js
+++ src/kafka/kafkaConsumer.js
@@ -29,10 +29,12 @@ module.exports = async function (ingressList) {
         //   value: message.value.toString(),
         // })
         const jsonMessage = JSON.parse(message.value)
+        const date = new Date(message.timestamp)
         logger.info("              ")
         logger.info(jsonMessage.object.metadata.name)
         logger.info(jsonMessage.object.metadata.creationTimestamp)
-        logger.info(message.timestamp)
+        logger.info(jsonMessage.cluster)
+        logger.info(date.toISOString())
         logger.info("              ")
         fetchKafkaIngresses(ingressList, jsonMessage)
         //logger.info(ingressList.size)
