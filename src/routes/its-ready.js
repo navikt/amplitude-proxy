5bea9c528f3692d243c5866d1c078f5b5a643add
JeremiahUy <jery_3000@hotmail.com>
2020-10-21 10:28:01 +0200

    ADD: check on ingress size before sending is ready status

--- src/routes/its-ready.js
+++ src/routes/its-ready.js
@@ -1,13 +1,21 @@
 const paths = require('../paths');
-const handler = async function(req, reply) {
-  reply.send('ok');
+const isReady = async function (req, reply, ingresses) {
+  if( ingresses.size > 2000) {
+    reply.send('ok');
+  } else {
+    reply.code(503).send("App is not ready, still collecting ingresses");
+  }
 };
 /**
  *
  * @type RouteOptions
  */
-module.exports = {
-  method: 'GET',
-  url: paths.ITS_READY,
-  handler
+module.exports = function (ingresses) {
+  return {
+    method: 'GET',
+    url: paths.ITS_READY,
+    handler: function (req, reply) {
+      isReady(req, reply, ingresses)
+    }
+  }
 };
