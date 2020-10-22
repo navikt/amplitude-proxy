6fb8f70cf652c62d7ef70894f36d2df1ee526a3a
JeremiahUy <jery_3000@hotmail.com>
2020-10-22 16:27:01 +0200

    ADD: test helper for generating kafka topics

---
+++ test-utils/generate-kafka-message.js
@@ -0,0 +1,26 @@
+const { Kafka } = require('kafkajs');
+const ExampleMessage = require('../examples/kafka-message.json')
+
+module.exports = async function () {
+  const kafka = new Kafka({ brokers: [process.env.KAFKA_BROKERS] })
+
+  const producer = kafka.producer()
+  await producer.connect()
+
+  for (x = 0; x < 2000; x++) {
+    const ingress = "https://test.test.test"
+    const key = "12345abcde"
+    const newKey = x + key
+    const newIngress = ingress + x
+    const newExample = ExampleMessage
+    newExample.object.spec.ingresses = [newIngress]
+    await producer.send({
+      topic: 'dataplattform.ingress-topic',
+      messages: [
+        { key: newKey, value: JSON.stringify(newExample) },
+      ],
+    })
+  }
+
+  await producer.disconnect()
+}
