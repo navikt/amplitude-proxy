440f31ec42805a37c6b3885a214f6c9797ac6b69
JeremiahUy <jery_3000@hotmail.com>
2020-10-30 14:32:42 +0100

    UPDATE: test for local url

--- src/filters/add-cluster-data.spec.js
+++ src/filters/add-cluster-data.spec.js
@@ -10,7 +10,6 @@ describe('add-cluster-data', function() {
   ingressException.forEach(data => ingressList.set(data.ingress, data))
 
   it('should enrich data when existing', function() {
-
     const testEvent = generateTestEvent('auto')
     testEvent.platform = 'https://www.nav.no/foo/bar?query=param';
     const outputEvents = addClusterData([testEvent], getIngressDate, ingressList);
@@ -19,6 +18,18 @@ describe('add-cluster-data', function() {
     assert.strictEqual(outputEvents[0].event_properties.hostname, 'www.nav.no');
     assert.strictEqual(outputEvents[0].event_properties.context, 'prod');
     assert.strictEqual(outputEvents[0].event_properties.pagePath, '/foo/bar');
+    assert.strictEqual(outputEvents[0].event_properties.creationTimestamp, undefined);
+  });
+
+  it('should enrich data when existing and is localhost', function() {
+    const testEvent = generateTestEvent('auto')
+    testEvent.platform = 'https://localhost:3000';
+    const outputEvents = addClusterData([testEvent], getIngressDate, ingressList);
+    assert.strictEqual(outputEvents[0].platform, 'Web');
+    assert.strictEqual(outputEvents[0].event_properties.namespace, 'local');
+    assert.strictEqual(outputEvents[0].event_properties.hostname, 'localhost');
+    assert.strictEqual(outputEvents[0].event_properties.context, 'local');
+    assert.strictEqual(outputEvents[0].event_properties.pagePath, '/');
     assert.strictEqual(outputEvents[0].event_properties.creationTimestamp, undefined);
   });
 
