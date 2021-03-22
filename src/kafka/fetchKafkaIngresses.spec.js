ce5fcae6bb54f089b6287a363e6056e1f56d745e
JeremiahUy <jeremiah.augie.uy@nav.no>
2021-03-22 10:13:28 +0100

    ADD: test for ignored list

--- src/kafka/fetchKafkaIngresses.spec.js
+++ src/kafka/fetchKafkaIngresses.spec.js
@@ -3,11 +3,13 @@ const fetchKafkaIngresses = require('./fetchKafkaIngresses')
 const getIngressExceptionPath = require('../data/ingressException-path')
 const ingressException = require(getIngressExceptionPath())
 const ignoreList = require('./ignoreList.json')
-const ignoreAppList = new Map()
-
-ignoreList.forEach(data => ignoreAppList.set(data.app, data))
 
 describe('Fetch Kafka ingresses', function () {
+
+  const ignoreAppList = new Map()
+
+  ignoreList.forEach(data => ignoreAppList.set(data.ingress, data))
+
   it('data should be transformed', function () {
 
     const ingresses = new Map()
@@ -115,7 +117,7 @@ describe('Fetch Kafka ingresses', function () {
   it('Should change is ready status to true when ingresses size is over 2000', function () {
 
     const ingresses = new Map()
-    let isReadyStatus = {status: false}
+    let isReadyStatus = { status: false }
 
     for (x = 0; x < 4000; x++) {
 
@@ -134,6 +136,41 @@ describe('Fetch Kafka ingresses', function () {
 
     fetchKafkaIngresses(ingresses, jsonTestData, isReadyStatus, ignoreAppList)
     assert.deepStrictEqual(isReadyStatus.status, true);
+
+  });
+
+  it('Should ignore app on ignored app list', function () {
+    const ingresses = new Map()
+    let isReadyStatus = { status: false }
+
+    ingresses.set("https://data.nais.preprod.local/apier", {
+      "app": "data-catalog-api-viewer",
+      "team": "dataplattform",
+      "namespace": "default",
+      "version": "2020-09-14--7aa9d15",
+      "context": "dev-fss",
+      "ingress": "https://data.nais.preprod.local/apier",
+      "creationTimestamp": "2020-05-12T12:36:38Z"
+    })
+
+    const jsonTestData = JSON.parse("{\"object\": {\"apiVersion\": \"nais.io/v1alpha1\", \"kind\": \"Application\", \"metadata\": {\"annotations\": {\"nais.io/deploymentCorrelationID\": \"3239908c-3bfe-41d8-be8e-85b8ca94cb61\"}, \"creationTimestamp\": \"2020-01-14T12:36:38Z\", \"generation\": 134, \"labels\": {\"team\": \"dataplattform\"}, \"name\": \"pensjon-pselv\", \"namespace\": \"dataplattform\", \"resourceVersion\": \"2204093870\", \"selfLink\": \"/apis/nais.io/v1alpha1/namespaces/dataplattform/applications/dakan-api-viewer\", \"uid\": \"dcd812e2-82de-4144-9cc7-52b6fcfdc372\"}, \"spec\": {\"image\": \"docker.pkg.github.com/navikt/dakan/dakan-api-viewer:2020-09-14--7aa9d15\", \"ingresses\": [\"https://www.nav.no/pselv\"], \"liveness\": {\"failureThreshold\": 30, \"initialDelay\": 5, \"path\": \"/apier/isAlive\", \"periodSeconds\": 5, \"timeout\": 1}, \"port\": 80, \"prometheus\": {\"enabled\": true, \"path\": \"/apier\"}, \"readiness\": {\"failureThreshold\": 30, \"initialDelay\": 5, \"path\": \"/apier/isReady\", \"periodSeconds\": 5, \"timeout\": 1}, \"replicas\": {\"cpuThresholdPercentage\": 70, \"max\": 2, \"min\": 2}, \"resources\": {\"limits\": {\"cpu\": \"400m\", \"memory\": \"512Mi\"}, \"requests\": {\"cpu\": \"50m\", \"memory\": \"128Mi\"}}, \"vault\": {\"enabled\": true, \"paths\": [{\"kvPath\": \"secret/dataplattform/datakatalog/preprod_fss\", \"mountPath\": \"/var/run/secrets/nais.io/common\"}]}}, \"status\": {\"correlationID\": \"3239908c-3bfe-41d8-be8e-85b8ca94cb61\", \"deploymentRolloutStatus\": \"complete\", \"rolloutCompleteTime\": 1600084931952750880, \"synchronizationHash\": \"b8515745917fb9d9\", \"synchronizationState\": \"RolloutComplete\", \"synchronizationTime\": 1600084891098839986}}, \"raw_object\": {\"apiVersion\": \"nais.io/v1alpha1\", \"kind\": \"Application\", \"metadata\": {\"annotations\": {\"nais.io/deploymentCorrelationID\": \"3239908c-3bfe-41d8-be8e-85b8ca94cb61\"}, \"creationTimestamp\": \"2020-01-14T12:36:38Z\", \"generation\": 134, \"labels\": {\"team\": \"dataplattform\"}, \"name\": \"pensjon-pselv\", \"namespace\": \"dataplattform\", \"resourceVersion\": \"2204093870\", \"selfLink\": \"/apis/nais.io/v1alpha1/namespaces/dataplattform/applications/dakan-api-viewer\", \"uid\": \"dcd812e2-82de-4144-9cc7-52b6fcfdc372\"}, \"spec\": {\"image\": \"docker.pkg.github.com/navikt/dakan/dakan-api-viewer:2020-09-14--7aa9d15\", \"ingresses\": [\"https://www.nav.no/pselv\"], \"liveness\": {\"failureThreshold\": 30, \"initialDelay\": 5, \"path\": \"/apier/isAlive\", \"periodSeconds\": 5, \"timeout\": 1}, \"port\": 80, \"prometheus\": {\"enabled\": true, \"path\": \"/apier\"}, \"readiness\": {\"failureThreshold\": 30, \"initialDelay\": 5, \"path\": \"/apier/isReady\", \"periodSeconds\": 5, \"timeout\": 1}, \"replicas\": {\"cpuThresholdPercentage\": 70, \"max\": 2, \"min\": 2}, \"resources\": {\"limits\": {\"cpu\": \"400m\", \"memory\": \"512Mi\"}, \"requests\": {\"cpu\": \"50m\", \"memory\": \"128Mi\"}}, \"vault\": {\"enabled\": true, \"paths\": [{\"kvPath\": \"secret/dataplattform/datakatalog/preprod_fss\", \"mountPath\": \"/var/run/secrets/nais.io/common\"}]}}, \"status\": {\"correlationID\": \"3239908c-3bfe-41d8-be8e-85b8ca94cb61\", \"deploymentRolloutStatus\": \"complete\", \"rolloutCompleteTime\": 1600084931952750880, \"synchronizationHash\": \"b8515745917fb9d9\", \"synchronizationState\": \"RolloutComplete\", \"synchronizationTime\": 1600084891098839986}}, \"cluster\": \"dev-fss\"}")
+
+    const expected = new Map()
+
+    expected.set("https://data.nais.preprod.local/apier", {
+      "app": "data-catalog-api-viewer",
+      "team": "dataplattform",
+      "namespace": "default",
+      "version": "2020-09-14--7aa9d15",
+      "context": "dev-fss",
+      "ingress": "https://data.nais.preprod.local/apier",
+      "creationTimestamp": "2020-05-12T12:36:38Z"
+    })
+
+    fetchKafkaIngresses(ingresses, jsonTestData, isReadyStatus, ignoreAppList)
+
+    assert.deepStrictEqual(ingresses, expected);
+    assert.deepStrictEqual(isReadyStatus.status, false);
 
   });
 });
