1af331a4619180df726593a51e378bb62a096a00
JeremiahUy <jeremiah.augie.uy@nav.no>
2022-05-24 16:26:56 +0200

    UPDATE: url and js_sdk

--- src/paths.js
+++ src/paths.js
@@ -6,7 +6,7 @@ module.exports = {
   INGRESSES: '/ingresses',
   ITS_ALIVE: '/health/is-alive',
   ITS_READY: '/health/is-ready',
-  JS_SDK: '/libs/amplitude-5.6.0-min.gz.js',
+  JS_SDK: '/libs/amplitude-8.18.2-min.gz.js',
   METRICS: '/metrics',
   SCHEMAS: '/schemas'
 };
