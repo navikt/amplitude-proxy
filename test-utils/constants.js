61ced9ea0a7daf5fbef66ca9eb68226e371bbb27
JeremiahUy <jeremiah.augie.uy@nav.no>
2022-05-24 17:18:33 +0200

    UPDATE: test project key

--- test-utils/constants.js
+++ test-utils/constants.js
@@ -1,5 +1,5 @@
 module.exports = {
   COMMON_USER_AGENT: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
-  TEST_PROJECT_KEY: '8f645054173ca9427912f8cc4181e2e9',
+  TEST_PROJECT_KEY: '7a25187655fc8d2d2f485e4391156997',
   TEST_NODE_ENV: 'test',
 };
