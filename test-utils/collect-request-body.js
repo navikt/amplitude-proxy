9181ff0c8d8ad54d41446cd24dde76c2d4bbddda
JeremiahUy <jeremiah.augie.uy@nav.no>
2022-11-14 13:41:31 +0100

    UPDATE: schema and add example for new event

--- test-utils/collect-request-body.js
+++ test-utils/collect-request-body.js
@@ -1,5 +1,6 @@
 const qs = require('querystring');
 const {TEST_PROJECT_KEY} = require('./constants');
+const exampleEvent = require('../examples/amplitude-v2-api-event');
 
 module.exports = (e, client) => {
   return qs.stringify({
