75e7c210507641705fd7225f27ac01b6dd5be9bf
JeremiahUy <jeremiah.augie.uy@nav.no>
2022-11-14 11:36:33 +0100

    UPDATE: request body for test

--- test-utils/collect-request-body.js
+++ test-utils/collect-request-body.js
@@ -1,10 +1,10 @@
 const qs = require('querystring');
 const {TEST_PROJECT_KEY} = require('./constants');
 
-module.exports = (events, client) => {
+module.exports = (e, client) => {
   return qs.stringify({
     client: client || TEST_PROJECT_KEY,
-    e: JSON.stringify(events),
+    e: JSON.stringify(e),
     events: null
   });
 };
