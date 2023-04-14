ab024b1c2a8a2806a91b586c277ce1e57d0b66a1
JeremiahUy <jeremiah.augie.uy@nav.no>
2023-04-14 10:32:20 +0200

    UPDATE: test for cleaning url with exceptions

--- src/utils/clean-url.spec.js
+++ src/utils/clean-url.spec.js
@@ -3,12 +3,12 @@ const constants = require('../constants');
 const cleanUrl = require('./clean-url');
 describe('clean-url', function() {
   it('urls should be cleaned', function() {
-    const cleanedUrl = cleanUrl('https://example.com/initial/1000Ro2Fi');
-    assert.strictEqual(cleanedUrl, 'https://example.com/initial/' + constants.REDACTED);
+    const cleanedUrl = cleanUrl('https://example.com/initial/1000Ro2Fi/nav123456789/test123456');
+    assert.strictEqual(cleanedUrl, 'https://example.com/initial/' + constants.REDACTED + '/nav123456789/test123456');
   });
   it('should remove 11 digit number from url', function () {
-    const cleanedUrl = cleanUrl('https://example.com/person/12345678901');
-    assert.strictEqual(cleanedUrl, 'https://example.com/person/' + constants.REDACTED)
+    const cleanedUrl = cleanUrl('https://example.com/person/12345678901/nav123456/test123456');
+    assert.strictEqual(cleanedUrl, 'https://example.com/person/' + constants.REDACTED + '/nav123456/test123456')
   });
   it('should not fail on bad input', function() {
     const cleanedUrl = cleanUrl(undefined);
