c740e0118af814eeb363486226f1d5a758a27b83
JeremiahUy <jeremiah.augie.uy@nav.no>
2023-04-14 10:34:40 +0200

    ADD: logic for cleaning url with exceptions

--- src/utils/clean-url.js
+++ src/utils/clean-url.js
@@ -5,13 +5,54 @@ const formats = [
   // id format https://www.nav.no/initial/1000Ro2Fi
   {regex: /\d[oiA-Z0-9]{8,}/g, replace: constants.REDACTED},
 ];
+
 module.exports = (url) => {
   let outputUrl = url;
   if(typeof url === 'string' || url instanceof String){
     formats.forEach(format => {
-      outputUrl = outputUrl.replace(format.regex, format.replace);
+      outputUrl = cleanUrlWithExceptions(outputUrl, format.regex, format.replace)
     });
   }
   return outputUrl;
 };
 
+
+// add unique replacers for exceptions
+// regex: regex for string value to find 
+// tempValue: temporary value to replaces matched value from regex 
+// replacers: regex to find tempValue to be able to replace them later
+const exceptions = [
+  // replaces a string that matches 'nav123456' with 'nav*', will be restored to original value when using cleanUrlWithExceptions
+  {regex: /nav[0-9]{6,}/g, tempValue: 'nav*', replacer: /nav\*/g},
+  // test replacer to check if method can handle multiple exceptions
+  {regex: /test[0-9]{6,}/g, tempValue: 'test*', replacer: /test\*/g}
+]
+
+// replaces the matched string with a temp value before cleaning url. After url is cleaned restores the temp value to its original state
+const cleanUrlWithExceptions = (outputUrl,formatRegex, formatReplacer) => {
+  
+  // list of found excluded string value with replacers on how to match and replace them to orignal value
+  let exceptionMatch = []
+
+  // replaces all exception strings with a temp value, so that id cleanup will not affect excluded strings
+  exceptions.forEach(ex => {
+    let match = outputUrl.match(ex.regex)
+
+    if(match && match.length > 0) {
+      outputUrl = outputUrl.replace(ex.regex, ex.tempValue);
+      exceptionMatch.push({originalValue: match, replacer: ex.replacer})
+    }
+  })
+
+  // url cleanup for ids
+  outputUrl = outputUrl.replace(formatRegex, formatReplacer);
+
+  // replaces temp values to its original value
+  if(exceptionMatch.length > 0) {
+    exceptionMatch.forEach(em => {
+      outputUrl = outputUrl.replace(em.replacer, em.originalValue)
+    })
+  }
+
+  return outputUrl
+}
