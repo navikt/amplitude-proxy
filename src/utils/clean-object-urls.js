a01490f8d04b1a306818bd07c7527d18f99d7f44
JeremiahUy <jeremiah.augie.uy@nav.no>
2023-08-15 12:04:21 +0200

    UPDATE: clean up code and test

--- src/utils/clean-object-urls.js
+++ src/utils/clean-object-urls.js
@@ -5,12 +5,12 @@ module.exports = (obj) => {
   Object.keys(cloneObj).forEach(function(key) {
     if(cloneObj[key] instanceof Object) {
       Object.keys(cloneObj[key]).forEach((nestedKey) => {
-        if (validUrl(cloneObj[key][nestedKey]) || nestedKey === 'pagePath') {
+        if (validUrl(cloneObj[key][nestedKey]) || nestedKey === 'pagePath' || (typeof cloneObj[key][nestedKey] === 'string' && cloneObj[key][nestedKey].startsWith('/'))) {
           cloneObj[key][nestedKey] = cleanUrl(cloneObj[key][nestedKey]);
         }
       })
     }
-    if (validUrl(cloneObj[key]) || key === 'pagePath') {
+    if (validUrl(cloneObj[key]) || key === 'pagePath' || (typeof cloneObj[key] === 'string' &&  cloneObj[key].startsWith('/'))) {
       cloneObj[key] = cleanUrl(cloneObj[key]);
     }
   });
