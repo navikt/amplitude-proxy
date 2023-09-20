832dabb98f729d71914748d57aa2bc947d53f8e2
Jeremiah Uy <jeremiah.augie.uy@nav.no>
2023-09-20 14:41:36 +0200

    UPDATE: to handle multiple instance of url in platform (#440)
    * UPDATE: to handle multiple instance of url in platform
    
    * UPDATE: shorten code
    
    * UPDATE: added comments
    
    * UPDATE: test
    
    * ADD: handling of platform when url is set on both event_properties and platform

--- src/filters/add-cluster-data.js
+++ src/filters/add-cluster-data.js
@@ -1,11 +1,32 @@
 const cleanUrl = require('../utils/clean-url');
-const addClusterData = (inputEvents, getIngressData, ingresses) => {
+const validUrl = require('../utils/valid-url');
+
+const addClusterData = (inputEvents, getIngressData, ingresses, usingNewSdk) => {
   const outputEvents = [];
   inputEvents.forEach(event => {
-    const cloneEvent = { ...event };
-    cloneEvent.platform = 'Web'; // Correct this back to the original.
+    const cloneEvent = { ...event };  
     cloneEvent.event_properties = event.event_properties || {};
-    let eventUrl = event.platform;
+    
+    let eventUrl;
+    //sjekker om platform i event_properties er satt og bruker den istedet
+    if(event.event_properties.platform) {
+      eventUrl = event.event_properties.platform
+      //sjekker om url ble også satt i platform og nullstiller det til 'Web'
+      if(validUrl(cloneEvent.platform)){
+        cloneEvent.platform = 'Web'
+      }
+    } 
+    //dersom platform i event_prorties er ikke satt vil koden hente url utifra om ny sdk-et er brukt
+    else {
+      if(usingNewSdk){ 
+        eventUrl = event.ingestion_metadata.source_name
+      } else {
+        //Setter platfrom til Web kun om du bruker den gamle måte (dvs. endrer setter platform under initialisering)
+        eventUrl = event.platform
+        cloneEvent.platform = 'Web'; // Correct this back to the original.
+      }
+    }
+
     if(eventUrl.includes('localhost')) {
       eventUrl = eventUrl.replace(/\:[0-9]+/g,'')
     }
@@ -17,6 +38,7 @@ const addClusterData = (inputEvents, getIngressData, ingresses) => {
         }
       });
     }
+
     const cleanedEventUrl = cleanUrl(eventUrl);
     const eventUrlObj = new URL(cleanedEventUrl);
     cloneEvent.event_properties.url = eventUrlObj.hostname + eventUrlObj.pathname;
--- src/routes/collect-auto.js
+++ src/routes/collect-auto.js
@@ -28,7 +28,8 @@ const customHandler = function (request, reply, ingresses) {
   let errors = []
   let apiKey;
   let usingNewSdk = false
-  if(request.body.events && request.body.events !== null) {
+
+  if (request.body.events && request.body.events !== null) {
     events = request.body.events
     apiKey = request.body.api_key;
     usingNewSdk = true
@@ -38,21 +39,35 @@ const customHandler = function (request, reply, ingresses) {
     errors = validateEvents(events);
     usingNewSdk = false
   }
-  
+
   events.forEach(event => {
-    if(event.platform === 'Web') {
-      if(event.ingestion_metadata && event.ingestion_metadata.source_name){
-        event.platform = event.ingestion_metadata.source_name
-        event.ingestion_metadata.source_name = undefined
+
+    //Sjekker om platform er satt i event_properties og validerer url
+    if (event.event_properties.platform && !validUrl(event.event_properties.platform)) {
+      errors.push('For auto-collect må window.location settes i \'platform\' eller være satt i \'event_properties\' i \'platform\'');
+    } 
+    
+    //dersom platform er ikke satt i event_properties vil den da hente validere url fra platform eller 
+    //ingestion_metadata ut ifra om du bruker den nye sdk-et
+    else {
+      if (usingNewSdk) {
+        if (!event.ingestion_metadata && !event.ingestion_metadata.source_name) {
+          errors.push('Når du bruker den nye skd for auto-collect må \'source name\' i \'ingestion metadata\' være satt til window.location');
+        } else {
+          if (!validUrl(event.ingestion_metadata.source_name)) {
+            errors.push('Når du bruker den nye skd for auto-collect må \'source name\' i \'ingestion metadata\' være satt til window.location');
+          }
+        }
       } else {
-        errors.push('Når du bruker den nye skd for auto-collect må \'source name\' i \'ingestion metadata\' være satt til window.location');
+        if (!validUrl(event.platform)) {
+          errors.push('For auto-collect må window.location settes i \'platform\' eller være satt i \'event_properties\' i \'platform\'');
+        }
       }
     }
-    if (!validUrl(event.platform)) {
-      errors.push('For auto-collect må \'platform\' være satt til window.location');
-    }
   });
-  const eventsWithClusterData = addClusterData(events, getIngressData, ingresses);
+
+
+  const eventsWithClusterData = addClusterData(events, getIngressData, ingresses, usingNewSdk);
   const appName = eventsWithClusterData[0].event_properties.app;
   const teamName = eventsWithClusterData[0].event_properties.team;
   const eventHostname = eventsWithClusterData[0].event_properties.hostname;
@@ -84,7 +99,7 @@ const customHandler = function (request, reply, ingresses) {
 
   } else {
     const eventsWithProxyData = addProxyData(eventsWithClusterData, process.env.NAIS_APP_IMAGE);
-    const eventsWithGeoData= addGeoData(eventsWithProxyData, request.ip, usingNewSdk);
+    const eventsWithGeoData = addGeoData(eventsWithProxyData, request.ip, usingNewSdk);
     const eventsWithUrlsCleaned = cleanEventUrls(eventsWithGeoData);
 
     forwardEvents(eventsWithUrlsCleaned, realApiKey, process.env.AMPLITUDE_URL).then(function (response) {
@@ -97,7 +112,7 @@ const customHandler = function (request, reply, ingresses) {
         reply.send(response.data);
       } else {
         collectCounter.labels('success', appName, teamName).inc();
-        if(request.body.events && request.body.events !== null) {
+        if (request.body.events && request.body.events !== null) {
           reply.send(response.data);
         } else {
           reply.send(constants.SUCCESS);
@@ -106,12 +121,12 @@ const customHandler = function (request, reply, ingresses) {
     }).catch(function (error) {
       collectCounter.labels('failed_proxy_events', appName, teamName).inc();
       let errorCode = 502
-      if(error.status) {
+      if (error.status) {
         errorCode = error.status
-      } else if ( error.response && error.response.status) {
+      } else if (error.response && error.response.status) {
         errorCode = error.response.status
       }
-      logger.error({...log(error.message), status_code: errorCode});
+      logger.error({ ...log(error.message), status_code: errorCode });
       reply.code(errorCode).send({
         statusCode: errorCode,
         message: 'Failed to proxy request',
